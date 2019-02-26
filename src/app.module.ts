dd6e4cff16a60dbb587b69a6f307f61bacdd689f
makadown <maseflores79@gmail.com>
2019-02-25 20:54:41 -0600

    Instalando orm para mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MensajesController } from './mensajes/mensajes.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'abc123',
+      database: 'sendmeapp_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController, MensajesController],
   providers: [AppService],
 })
